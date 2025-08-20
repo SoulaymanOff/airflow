7a093dbb2ad092ab3b5fde840d8ed626b5b0c5e4
Soulayman Chedid <soulayman.chedid@outscale.com>
2025-08-20 13:16:07 +0200

    modify

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+from datetime import datetime
+from airflow.decorators import dag, task
+
+@dag(
+    schedule="@daily",              # cron or presets like @hourly, @daily
+    start_date=datetime(2024, 1, 1),
+    catchup=False,                  # run only new schedules
+    default_args={"retries": 1},
+    tags=["demo"],
+)
+def hello_world():
+    @task
+    def say_hello():
+        print("Hello from Airflow!")
+
+    say_hello()
+
+dag = hello_world()
